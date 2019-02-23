41c8768edd65e7b7dc971d0c5de1fc0a7fb725e7
hafijul233 <hafijul233@gmail.com>
2019-02-24 03:20:29 +0600

    Basic Alrt Respone Set

---
+++ validator.php
@@ -0,0 +1,24 @@
+<?php
+    //Array ( [email_address] => hafijul233@gmail.com [password] => 123456 [rememberMe] => remember-me ) 
+    
+    $email = $_POST['email_address'];
+    $password = $_POST['password'];
+    $response = array();
+    $errors = array();
+    
+    if(empty($email) || empty($password)) {
+        $response['errors'] = "One or More Fields are Empty";
+    }
+    else {
+        if($email == "hafijul233@gmail.com" && $password == "123456") {
+            $response['errors'] = NULL;
+            $response['msg'] = "Match Found";
+        }
+        else {
+            $response['errors'] = NULL;
+            $response['msg'] = "Match Not Found";
+        }
+    }
+        
+    echo json_encode($response);
+    
